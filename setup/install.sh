773bc82261547aa65e00cf6ec4c55e40ec299179
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-25 13:59:46 +0530

    rev

--- setup/install.sh
+++ setup/install.sh
@@ -1,8 +1,11 @@
 create_symlinks() {
+  echo " Installing - git.plugin"
   local root=$(nex _plugin git)
   local base=$root/src/config
   ln -nfs $base/_gitconfig $HOME/.gitconfig
   ln -nfs $base/_gitignore_global $HOME/.gitignore_global
+  echo " symlinked .gitconfig, .gitignore_global"
+  echo
 }
 
 create_symlinks
